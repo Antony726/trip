9fca2a2d8fcf7e99ac0e2500b28653fa0435ea22
Antony <alexantony726@gmail.com>
2026-09-18 21:58:43 +0530

    Update base URL for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Crucial for GitHub Pages relative asset paths!
+  base: '/trip/', // Set to your exact GitHub repo name for GitHub Pages
   build: {
     outDir: 'dist',
   }
